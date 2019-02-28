c6ca825ead6420158dbc5a3516293fe5b144e8e5
josepeduard <j.e.garriga.obach@gmail.com>
2019-02-28 09:50:06 +0100

    first

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 
-const express = require('express')
-const hbs = require('hbs')
-const app = express()
-const path = require('path')
-const PunkAPIWrapper = require('punkapi-javascript-wrapper')
-const punkAPI = new PunkAPIWrapper()
+const express = require('express');
+// const hbs = require('hbs');
+const app = express();
+const path = require('path');
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+const punkAPI = new PunkAPIWrapper();
 
-app.set('view engine', 'hbs')
-app.set('views', __dirname + '/views')
-app.use(express.static(path.join(__dirname, 'public')))
-
-
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
-  res.render('index')
-})
+  res.render('index');
+});
 
+// -- 404 and error handler
 
+// NOTE: requires a views/not-found.ejs template
+app.use((req, res, next) => {
+  res.status(404);
+  res.render('not-found');
+});
 
-app.listen(3000)
+// NOTE: requires a views/error.ejs template
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500);
+    res.render('error');
+  }
+});
+
+app.listen(3000);
